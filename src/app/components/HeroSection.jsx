1905f8c933424835098eb9a67d65ca6b549b2dcc
Hao Li <hli197@my.bcit.ca>
2024-05-02 04:25:14 -0700

    added Hero-Section to Protflio Web.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,9 +1,63 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+
+import { TypeAnimation } from "react-type-animation";
 
 export const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white">Hello, I&apos;m Allen</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-600 to-pink-400 ">
+              Hello, I&apos;m{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Allen",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "web developer",
+                1000,
+                "Devops specialist",
+                1000,
+                "Software developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] tetx-base sm:text-lg mb-6 lg:text-xl">
+            Keep your face always toward the sunshine, and shadows will fall
+            behind you.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-yellow-500 via-red-500 to bg-pink-500 text-black">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-yellow-500 via-red-500 to bg-pink-500 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Resume
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="Hero Image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={250}
+              height={250}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
