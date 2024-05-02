66b5e682113be8d0c952148efb44ce32c75dbebf
Hao Li <hli197@my.bcit.ca>
2024-05-02 02:05:01 -0700

    First commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">Hello, I&apos;m Allen</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
