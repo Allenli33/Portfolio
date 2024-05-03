6708c2024e79ec9279de0a5396996d764df9bdbe
Hao Li <hli197@my.bcit.ca>
2024-05-03 00:02:50 -0700

    finshed navbars to the page.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
@@ -13,22 +14,25 @@ const navLinks = [
     title: "Projects",
     path: "#projects",
   },
-
   {
     title: "Contact",
     path: "#contact",
   },
 ];
 
-export const Navbar = () => {
+const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
-    <nav className="fixed-top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href={"/"} className="text-5xl text-white font-semibold">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           LOGO
         </Link>
-        <div className=" mobile:menu block md:hidden">
+        <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
@@ -55,6 +59,7 @@ export const Navbar = () => {
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };
