844d2e119346cb565578f81c6ec2242ba2ad4165
Hao Li <hli197@my.bcit.ca>
2024-05-06 03:41:18 -0700

    fixing issues

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,14 +1,14 @@
 "use client";
-import React, { useTransition, useState } from 'react';
-import Image from 'next/image';
-import TabButton from './TabButton';
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
 
 const TAB_DATA = [
   {
     title: "Skills",
     id: "skills",
     content: (
-      <ul style={{ listStyleType: 'none' }} className="list-disc pl-2">
+      <ul style={{ listStyleType: "none" }} className="list-disc pl-2">
         <li>Node.js</li>
         <li>Python</li>
         <li>MySQL</li>
@@ -24,7 +24,7 @@ const TAB_DATA = [
     title: "Education",
     id: "education",
     content: (
-      <ul style={{ listStyleType: 'none' }} className="list-disc pl-2">
+      <ul style={{ listStyleType: "none" }} className="list-disc pl-2">
         <li>BCIT: Computer Information Technology</li>
         <li>Kwantlen Polytechnic University</li>
       </ul>
@@ -34,7 +34,7 @@ const TAB_DATA = [
     title: "Certifications",
     id: "certifications",
     content: (
-      <ul style={{ listStyleType: 'none' }} className="list-disc pl-2">
+      <ul style={{ listStyleType: "none" }} className="list-disc pl-2">
         <li>AWS Cloud Practitioner in progress</li>
         <li>CompTIA S+ in progress</li>
       </ul>
@@ -45,27 +45,35 @@ const TAB_DATA = [
 const AboutSection = () => {
   const [tab, setTab] = useState(TAB_DATA[0].id);
   const [isPending, startTransition] = useTransition();
-  const handleTabChange = (id) => { 
+  const handleTabChange = (id) => {
     startTransition(() => {
       setTab(id);
     });
-  }
+  };
 
   return (
     <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-3 xl:gap-16 sm:py-12 xl:px-12">
         <Image
           src="/images/about-image.png"
           alt="About Image"
-          width={600}
-          height={600}
+          width={500}
+          height={500}
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-pink-300 lg:text-lg">
-          I recently graduated from BCIT&apos;s Computer Information Technology diploma, specializing on web programming and web application design. My technical skills include JavaScript, HTML, CSS, and deep knowledge of Node.js and Flask. My expertise include algorithm creation, database management, and cloud services, particularly AWS. Furthermore, I have hands-on expertise with infrastructure as code and automation technologies such as Terraform and Ansible, which has improved my capacity to manage complicated web infrastructures and build scalable apps.
+            I recently graduated from BCIT&apos;s Computer Information
+            Technology diploma, specializing on web programming and web
+            application design. My technical skills include JavaScript, HTML,
+            CSS, and deep knowledge of Node.js and Flask. My expertise include
+            algorithm creation, database management, and cloud services,
+            particularly AWS. Furthermore, I have hands-on expertise with
+            infrastructure as code and automation technologies such as Terraform
+            and Ansible, which has improved my capacity to manage complicated
+            web infrastructures and build scalable apps.
           </p>
-          <div className='flex flex-row mt-8'>
+          <div className="flex flex-row mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export const TabButton = ({ active, selectTab, children}) => {
-       const buttonClasses = active ? 'text-white border-b border-purple-500' : 'mr-3 font-semibold hover:text-red-800 text-[#ADB7BE] border-b border-purple-500'
+export const TabButton = ({ active, selectTab, children }) => {
+  const buttonClasses = active
+    ? "text-white border-b border-purple-500"
+    : "mr-3 font-semibold hover:text-red-800 text-[#ADB7BE] border-b border-purple-500";
   return (
     <button onClick={selectTab}>
-        <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
-    
     </button>
-  )
-}
+  );
+};
 
-export default TabButton
+export default TabButton;
