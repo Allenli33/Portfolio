a80a19a73a1b0653db91e4e79a3d42cb185f8408
Hao Li <hli197@my.bcit.ca>
2024-05-10 03:13:20 -0700

    added achievements section.

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,16 @@ import AboutSection from "./components/AboutSection";
 import Projects from "./components/Projects";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import AchievementsSection from "./components/Achievements";
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        {/* <AchievementsSection /> */}
         <AboutSection />
         <Projects />
         <EmailSection />
