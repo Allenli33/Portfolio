f8c0b05124557577882fd02b94822cce96e70b82
Hao Li <hli197@my.bcit.ca>
2024-05-10 03:07:57 -0700

    finished email section.

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+  const [emailSubmitted, setEmailSubmited] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent successfully!");
+      setEmailSubmited(true);
+    }
+  };
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-6 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
 
       <div className="z-10">
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -19,6 +48,7 @@ const EmailSection = () => {
               Your Email
             </label>
             <input
+              name="email"
               type="email"
               id="email"
               required
@@ -34,6 +64,7 @@ const EmailSection = () => {
               Subject
             </label>
             <input
+              name="subject"
               type="text"
               id="subject"
               required
@@ -61,6 +92,11 @@ const EmailSection = () => {
           >
             Send
           </button>
+          {emailSubmitted && (
+            <p className="text-green-500 text-sm mt-2">
+              Message sent successfully!
+            </p>
+          )}
         </form>
       </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import Projects from "./components/Projects";
 import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
@@ -13,7 +14,9 @@ export default function Home() {
         <AboutSection />
         <Projects />
         <EmailSection />
+
       </div>
+      <Footer />
     </main>
   );
 }
