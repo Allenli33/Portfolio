55a668510f45c1bdb237a1a0cdd53e3fb7a73720
Hao Li <hli197@my.bcit.ca>
2024-05-15 03:12:44 -0700

    fixed email issues.

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -9,28 +9,31 @@ const projectsData = [
   {
     id: 1,
     title: " Python Pygame Flappy Patrick",
-    description: "Project 1 description",
+    description:
+      "Developed a Flappy Bird-inspired game, titled 'Flappy Patrick,' which incorporated real-time scoring and dynamic obstacles to enhance gameplay.",
     image: "/images/projects/3.jpg",
     tag: ["All", "Python:Pygame"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/Allenli33/2515_Final_Project",
     previewUrl: "/",
   },
   {
     id: 2,
     title: "Library Microservices",
-    description: "Project 2 description",
+    description:
+      "Architected a distributed library management system using microservices, focusing on scalability and resilience.Enabled efficient microservice communication with Kafka and used Docker for consistent deployments, contributing to a robust and isolated environment",
     image: "/images/projects/6.jpg",
     tag: ["All", "Microservice"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/Allenli33/acit3855_lab7",
     previewUrl: "/",
   },
   {
     id: 3,
     title: "Network Provisioning with Terraform and Ansible",
-    description: "Project 3 description",
+    description:
+      "Orchestrated cloud-based infrastructure deployment on AWS using Terraform for infrastructure as code practices and Ansible for automation of configuration and deployment tasks",
     image: "/images/projects/13.jpg",
     tag: ["All", "Network Provisioning"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/Allenli33/ACIT4640",
     previewUrl: "/",
   },
 ];
